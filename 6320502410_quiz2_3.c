ea5ef81f4e28398b5a0b1c5d52b9946e69ab458e
Nitiphat2 <moozknot@gmail.com>
2021-03-25 18:34:49 +0700

    aass

--- 6320502410_quiz2_3.c
+++ 6320502410_quiz2_3.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 int main()
 {
-
+    int n,m;
+    scanf("%d",&n);
+    int i,j,k,a,b,c;
+    for(i=1;i<=n;i++)
+    {
+        k=i;
+        for(j=0;k>=10;j++)
+        {
+            k=k/10;
+        }
+        printf("%d",j);
+    }
 }
